e434125e44b4cd11b5ab8f03a25785a3b201451c
NielsAveniq <niels.vonschack@aveniq.ch>
2022-04-01 11:16:41 +0000

    controllerAnzeigedesFragments

--- webapp/controller/View.controller.js
+++ webapp/controller/View.controller.js
@@ -1,15 +1,43 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/Fragment"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, Fragment) {
         "use strict";
 
         return Controller.extend("ipa.controller.View", {
             onInit: function () {
 
+                // Set the initial form to be the display one
+                this._formFragments = {};
+			    this._showFormFragment("Display");
+            },
+
+            _getFormFragment: function (sFragmentName) {
+                var pFormFragment = this._formFragments[sFragmentName],
+                    oView = this.getView();
+    
+                if (!pFormFragment) {
+                    pFormFragment = Fragment.load({
+                        id: oView.getId(),
+                        name: "ipa.view." + sFragmentName
+                    });
+                    this._formFragments[sFragmentName] = pFormFragment;
+                }
+    
+                return pFormFragment;
+            },
+
+            _showFormFragment : function (sFragmentName) {
+                var oPage = this.byId("page");
+    
+                oPage.removeAllContent();
+                this._getFormFragment(sFragmentName).then(function(oVBox){
+                    oPage.insertContent(oVBox);
+                });
             }
         });
     });
